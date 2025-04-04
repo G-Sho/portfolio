1144e5646ac96534429eb9d6425f316e19a55267
G-Sho <shonoshin.goto@gmail.com>
2025-04-04 14:07:24 +0900

    改行し忘れ，修正

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -13,7 +13,7 @@ const Career: React.FC = () => {
         </Box>
         <Box display='flex' justifyContent='center' p={1}>
           <Typography variant='body1' align='left'>
-            2021/04 ～ 2025/03 工学院大学 情報学部 情報通信工学科
+            2021/04 ～ 2025/03 工学院大学 情報学部 情報通信工学科<br />
             2025/04 ～ 2027/03(卒業予定) 工学院大学 大学院 工学研究修士課程 電気電子工学専攻
           </Typography>
         </Box>
